0b924c1cef03c330aaac81e1dd613c62a1f68221
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-08-31 08:55:46 -0400

    main1 and main2 consolidated into main.py

--- practical/main.py
+++ practical/main.py
@@ -18,18 +18,25 @@ def start_function (root_project, root_0D, arg, main_container):
 
 def components_to_include_in_project (root_project, root_0D, reg):
     zd.register_component (reg, zd.Template ("Unit A", None, unitA))
-    zd.register_component (reg, zd.Template ("Unit C", None, unitB))
-
-def unitB (reg, owner, name, template_data):      
-    name_with_id = zd.gensym ("?")
-    return zd.make_leaf (name=name_with_id, owner=owner, instance_data=None, handler=unitB_handler)
-def unitB_handler (eh, msg):
-    print (f"unit C got {msg.datum.srepr ()} on port {msg.port}")
+    zd.register_component (reg, zd.Template ("Unit B", None, unitB))
+    zd.register_component (reg, zd.Template ("Unit C", None, unitC))
 
 def unitA (reg, owner, name, template_data):      
     name_with_id = zd.gensym ("?")
     return zd.make_leaf (name=name_with_id, owner=owner, instance_data=None, handler=unitA_handler)
 def unitA_handler (eh, msg):
     zd.send (eh, "f", zd.new_datum_int (42), msg)
+
+def unitB (reg, owner, name, template_data):      
+    name_with_id = zd.gensym ("?")
+    return zd.make_leaf (name=name_with_id, owner=owner, instance_data=None, handler=unitB_handler)
+def unitB_handler (eh, msg):
+    print (f"unit B got {msg.datum.srepr ()} on port {msg.port}")
+
+def unitC (reg, owner, name, template_data):      
+    name_with_id = zd.gensym ("?")
+    return zd.make_leaf (name=name_with_id, owner=owner, instance_data=None, handler=unitC_handler)
+def unitC_handler (eh, msg):
+    print (f"unit C got {msg.datum.srepr ()} on port {msg.port}")
 
 main ()
